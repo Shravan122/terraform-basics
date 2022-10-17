266a771e59e0234511be544c526605e842455124
Shravan <shravan.kumbar@gmail.com>
2022-10-17 20:41:55 +0530

    Corrected

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo" {
-  ami           = ami-00ef5054fab40ba04
+  ami           = "ami-00ef5054fab40ba04"
   instance_type = "t3.micro"
 
   tags = {
