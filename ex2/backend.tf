68c680102200891a810b15316e62592a9344bc17
Shravan <shravan.kumbar@gmail.com>
2022-10-20 13:35:48 +0530

    Added

--- ex2/backend.tf
+++ ex2/backend.tf
@@ -1,1 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "b50-tf-state-bucket"
+    key    = "module/env/terrafom.tfstate"
+    region = "us-east-1"
+   }
+}
 
+provider "aws" {
+    region = "us-east-1"
+}
